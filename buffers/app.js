4d00837ed828ecc90a26ea1cec8a13c91b2f2527
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-11-11 12:16:23 +0100

    Understand buffer and memory allocation

---
+++ buffers/app.js
@@ -0,0 +1,15 @@
+const {Buffer} = require("buffer");
+
+const memoryContainer = Buffer.alloc(4)
+
+memoryContainer[0] = 0x00
+memoryContainer[1] = 0x42
+memoryContainer.writeInt8(-0x43, 2);
+memoryContainer[3] = 0xff
+
+console.log(memoryContainer)
+console.log(memoryContainer[0]);
+console.log(memoryContainer[1]);
+console.log(memoryContainer.readInt8(2));
+console.log(memoryContainer[3]);
+   
