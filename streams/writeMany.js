dbc24cd6fb7d41ea9174791e5edf96e5708f5e8d
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-11-26 21:35:47 +0100

    update on stream, fixing Memory issues

---
+++ streams/writeMany.js
@@ -0,0 +1,127 @@
+
+//Using promises
+// const fs = require('node:fs/promises');
+
+// (async(path, data) => {
+//     try {
+//         console.time('writeMany');
+//         const fileHandle = await fs.open("./test.txt", "w")
+//         for (let i = 0; i < 1000000; i++) {
+//              fileHandle.write(` ${i} `);
+//         }
+//         console.timeEnd("writeMany");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();
+
+
+//Execution Time: 8.3s
+//CPU Usage: 100% (one core)
+//Memory Usage: 50MB
+//Using callback
+// const fs = require("node:fs");
+
+// (async (path, data) => {
+//     console.time("writeMany");
+//     fs.open("./test.txt", "w", (err, fileHandle) => {
+//       for (let i = 0; i < 1000000; i++) {
+//         fs.writeSync(fileHandle, ` ${i} `);
+//       }   
+//       console.timeEnd("writeMany")
+//     });
+// })();
+
+
+//Dont do it this way 
+//Execution Time: 8.3s
+//CPU Usage: 100% (one core)
+//Memory Usage: 50MB
+//Using Stream
+// const fs = require("node:fs");
+
+// (async (path, data) => {
+//     console.time("writeMany");
+//     const fileHandle = await fs.promises.open("./test.txt", "w")
+
+//     const stream = fileHandle.createWriteStream();
+    
+//     for(let i = 0; i < 1000000; i++){
+//         const buff = Buffer.from(` ${i}`, "utf-8");
+//         stream.write(buff);
+//     }
+
+//     console.timeEnd("writeMany")
+    
+// })();
+
+// const fs = require("node:fs/promises");
+
+// (async () => {
+//     // console.time("writeMany");
+//     const fileHandle = await fs.open("./test.txt", "w")
+
+//     const stream = fileHandle.createWriteStream();
+
+//     console.log(stream.writableHighWaterMark)
+//     console.log(stream.writableLength)
+
+//     // const buff = Buffer.from("This is a content", "utf-8");
+
+//     // stream.write(buff)
+//     // console.log(buff)
+//     // console.log(stream.writableLength)
+
+//     // 8 bits = 1 byte
+//     // 1000 bytes = 1 kilobyte
+//     // 1000 kilobytes = 1 megabytes
+//     const buff = Buffer.alloc(10000000, 10);
+//     console.log(buff)
+
+
+//     // console.timeEnd("writeMany")
+    
+// })();
+
+const fs = require("node:fs/promises");
+
+(async () => {
+    // console.time("writeMany");
+    const fileHandle = await fs.open("./test.txt", "w")
+
+    const stream = fileHandle.createWriteStream();
+
+    console.log(stream.writableHighWaterMark)
+
+
+    // const buff = Buffer.from("This is a content", "utf-8");
+
+    // stream.write(buff)
+    // console.log(buff)
+    // console.log(stream.writableLength)
+
+    // 8 bits = 1 byte
+    // 1000 bytes = 1 kilobyte
+    // 1000 kilobytes = 1 megabytes
+    // const buff = Buffer.alloc(10000000, 10);
+    // console.log(buff)
+
+    let i = 0;
+    const writeMany = () => {
+        while(i < 1000000){
+        const buff = Buffer.from(` ${i} `, "utf-8");
+            if(!stream.write(buff)) break
+            i++;
+        }
+}
+
+    writeMany();
+    
+    stream.on("drain", () => {
+        writeMany();
+    })
+
+    // console.timeEnd("writeMany")
+    
+})();
+
